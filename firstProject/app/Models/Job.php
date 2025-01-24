c01b725ee2791d2fda769350a6ab7cfb307a3f5c
Mihai Minaileanu <minaileanurm@yahoo.com>
2025-01-24 22:12:13 +0200

    Eloquent introduction - pivot tables, eager loading, pagination/ seeding

--- firstProject/app/Models/Job.php
+++ firstProject/app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Arr;
 
 class Job extends Model
@@ -12,4 +13,14 @@ class Job extends Model
 
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->BelongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey:"job_listing_id");
+    }
 }
--- firstProject/resources/views/jobs.blade.php
+++ firstProject/resources/views/jobs.blade.php
@@ -3,13 +3,21 @@
         Jobs listings
     </x-slot:heading>
     
-    <ul>
+    <div class="space-y-4">
         @foreach($jobs as $job)
-            <li>
-                <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:underline">
-                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year
+            <a>
+                <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200">
+                    <div class="font-bold text-blue-500 text-sm">
+                        {{ $job->employer->name}}
+                    </div>
+                    <div>
+                        <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year
+                    </div>
                 </a>
-            </li>
+            </a>
         @endforeach
-    </ul>
+        <div>
+            {{ $jobs->links() }}
+        </div>
+    </div>
 </x-layout>
