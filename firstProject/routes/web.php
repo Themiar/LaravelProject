682af53a9f4eedf814558cbb635cfd57259f9b55
Mihai Minaileanu <minaileanurm@yahoo.com>
2025-01-25 15:38:07 +0200

    Forms, validation, crud, routes

--- firstProject/routes/web.php
+++ firstProject/routes/web.php
@@ -1,26 +1,24 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/','home');
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(10);
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs','index');
+//     Route::get('/jobs/create','create');
+//     Route::get('/jobs/{job}','show');
+//     Route::post('/jobs','store');
+//     Route::get('/jobs/{job}/edit','edit');
+//     Route::patch('/jobs/{job}','update');
+//     Route::delete('/jobs/{job}','destroy');
+// });
 
-    return view('jobs',[
-        'jobs' => $jobs
-    ]);
-});
+// Route::resource('jobs', JobController::class, [
+//     'except' => ['edit']
+// ]);
 
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-    return view('job', ['job' => $job]);
-});
+Route::resource('jobs', JobController::class);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
